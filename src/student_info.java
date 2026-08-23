a4d5ecdef1669c2b206c4c3ecbac6d7f7fb79db7
M B NANDEESH <269104320+MBNandeesh@users.noreply.github.com>
2026-08-23 18:47:10 +0530

    Day 1: Java fundamentals

---
+++ src/student_info.java
@@ -0,0 +1,15 @@
+public class student_info {
+    public static void main(String[] args) {
+        String name = "Nandeesh";
+        double cgpa = 9.6;
+        String college = "Nagarjuna";
+        int age = 20;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(college);
+        System.out.println(cgpa);
+
+
+    }
+}
---
+++ src/tiny.java
@@ -0,0 +1,12 @@
+public class tiny {
+    public static void main(String[] args) {
+        int age = 20;
+        double height = 5.8;
+        String name = "Kiyorin";
+        char alpha ='A';
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(alpha);
+    }
+}
